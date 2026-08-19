5ab549ad1ab625cf24e0dc4ee16e333b390d9dd1
Muneeb <muhaamadmuneeb777@gmail.com>
2026-08-19 20:25:43 +0500

    flutter project

---
+++ lib/Onboarding/splash_activity.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashActivity extends StatelessWidget {
+  const SplashActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(backgroundColor: Color(0xFF108244)));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_final_project/Onboarding/splash_activity.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashActivity(),
     );
   }
 }
